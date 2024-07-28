9c4b93589e0eeabac3df3ce9197799885ab68037
HP <HP@DESKTOP-4G5L3NT>
2024-07-28 16:45:20 +0530

    add file

--- src/edu/ijse/library/Main.java
+++ src/edu/ijse/library/Main.java
@@ -16,6 +16,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
+        System.out.println("");
         new CategoryView().setVisible(true);
     }
     
